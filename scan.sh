20aa4b87fd106950180cea5df6974db4a1a08e87
hobbitpl <minimalnie@gmail.com>
2017-05-20 09:21:04 +0200

    add scan.sh

---
+++ scan.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# define your scan output directory
+scanPath='/home/YourHomeDirectory/stuff/temp/tempSkan'
+
+id=$$
+data=`date +%Y%m%d`
+hour=`date +%H%M%S`
+
+echo "Ile skanów? [podaj liczbê & ENTER]: "
+read ile
+
+x=0
+while [ $x -lt $ile ] ;do
+#echo "sas" > scan.$data.$hour.$x.jpg
+filename=scan'_'$data'_'$hour'_'$x.jpg
+scanimage --resolution 150 --mode Gray --format=jpeg > $scanPath/$filename
+convert -rotate 0 $scanPath/$filename $scanPath/$filename
+chmod 664 $scanPath/$filename
+let x=x+1
+done
